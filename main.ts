6b4238c754c647059fb5a25fd460a889afbd1153
BeslijaHana <125749415+BeslijaHana@users.noreply.github.com>
2023-03-02 23:39:30 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,47 @@
+input.onButtonPressed(Button.A, function () {
+    while (sek <= 10 && sek >= 1) {
+        basic.pause(1000)
+        basic.showNumber(sek)
+        sek += -1
+    }
+    for (let x = 0; x <= 3; x++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+        basic.clearScreen()
+    }
+})
+let sek = 0
+sek = 10
